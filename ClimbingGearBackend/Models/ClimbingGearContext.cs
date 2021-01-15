62ea49ca25f1c2ce9d4e1e1e6a69164d83496351
Deryck Jackson <Dgjackson01@gmail.com>
2021-01-15 08:23:12 -0800

    Gear (#1)
    * add Gear model and controller, add DBcontext
    
    * add GearController and tests
    
    * Fix indentations, make test changes per PR and add formatting options

---
+++ ClimbingGearBackend/Models/ClimbingGearContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ClimbingGearBackend.Models
+{
+  public class ClimbingGearContext : DbContext
+  {
+    public ClimbingGearContext(DbContextOptions<ClimbingGearContext> options)
+      : base(options)
+    {
+    }
+
+    public DbSet<Gear> Gear { get; set; }
+  }
+}
--- ClimbingGearBackend/Models/Gear.cs
+++ ClimbingGearBackend/Models/Gear.cs
@@ -2,12 +2,26 @@ namespace ClimbingGearBackend.Models
 {
   public class Gear
   {
-    public int Id { get; set; }
+    public long Id { get; set; }
     public string Name { get; set; }
-    public int Qty { get; set; }
-    public string Desc { get; set; }
+    public string Description { get; set; }
     public string Brand { get; set; }
     public int WeightGrams { get; set; }
+    public int LengthMM { get; set; }
+    public int WidthMM { get; set; }
+    public int DepthMM { get; set; }
+    public bool Locking { get; set; }
+    public string Secret { get; set; }
+  }
+
+  public class GearDTO
+  {
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Brand { get; set; }
+    public int WeightGrams { get; set; }
+    public int LengthMM { get; set; }
     public int WidthMM { get; set; }
     public int DepthMM { get; set; }
     public bool Locking { get; set; }
