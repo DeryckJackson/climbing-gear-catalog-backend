4ad30b856ac04c5a97d49c532f42bc0873ce1861
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2021-01-25 14:33:51 -0800

    change UserId in UserGear to string type

---
+++ ClimbingGearBackend/Migrations/20210125222810_Initial.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace ClimbingGearBackend.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Gear",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Brand = table.Column<string>(type: "text", nullable: true),
+                    WeightGrams = table.Column<int>(type: "integer", nullable: false),
+                    LengthMM = table.Column<int>(type: "integer", nullable: false),
+                    WidthMM = table.Column<int>(type: "integer", nullable: false),
+                    DepthMM = table.Column<int>(type: "integer", nullable: false),
+                    Locking = table.Column<bool>(type: "boolean", nullable: false),
+                    Secret = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gear", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserGear",
+                columns: table => new
+                {
+                    UserGearId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    GearId = table.Column<long>(type: "bigint", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: true),
+                    Quantity = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGear", x => x.UserGearId);
+                    table.ForeignKey(
+                        name: "FK_UserGear_Gear_GearId",
+                        column: x => x.GearId,
+                        principalTable: "Gear",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGear_GearId",
+                table: "UserGear",
+                column: "GearId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserGear");
+
+            migrationBuilder.DropTable(
+                name: "Gear");
+        }
+    }
+}
